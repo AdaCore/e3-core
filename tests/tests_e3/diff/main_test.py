692e8df810805b3deadd121e7dbb1d08f4eda8c2
Nicolas Roche <roche@adacore.com>
2016-04-21 15:23:47 +0100

    Add new tests on e3.fs and e3.diff
    Change-Id: Ie907532328379a559b119a03d06c5c57b8cc95bc

--- tests/tests_e3/diff/main_test.py
+++ tests/tests_e3/diff/main_test.py
@@ -1,6 +1,38 @@
 import e3.diff
+import e3.fs
+import os
+import tempfile
 
 
 def test_non_existing():
     """Check that a non existing file will be considered as null string"""
     assert e3.diff.diff('foo1', 'foo2') == ''
+
+
+def test_patch():
+    test_dir = os.path.dirname(__file__)
+    file_to_patch = os.path.join(test_dir, 'file_to_patch.orig.txt')
+    file_after_patch = os.path.join(test_dir, 'file_to_patch.new.txt')
+    file_after_patch2 = os.path.join(test_dir, 'file_to_patch.new2.txt')
+    file_patch = os.path.join(test_dir, 'patch.txt')
+    file_patch2 = os.path.join(test_dir, 'patch2.txt')
+
+    tempd = tempfile.mkdtemp()
+    try:
+        e3.fs.cp(file_to_patch, tempd)
+        e3.diff.patch(file_patch, tempd)
+
+        with open(os.path.join(tempd, 'file_to_patch.orig.txt')) as fd:
+            output = fd.readlines()
+
+        with open(file_after_patch) as fd:
+            expected = fd.readlines()
+
+        assert e3.diff.diff(expected, output) == ''
+
+        e3.diff.patch(file_patch2, tempd,
+                      discarded_files=['dummy_to_patch.new.txt'])
+        assert e3.diff.diff(os.path.join(tempd, 'file_to_patch.orig.txt'),
+                            file_after_patch2) == ''
+    finally:
+        e3.fs.rm(tempd, True)
--- tests/tests_e3/fs/main_test.py
+++ tests/tests_e3/fs/main_test.py
@@ -2,6 +2,7 @@ import e3.hash
 import e3.fs
 import os
 import tempfile
+import pytest
 
 
 def test_cp():
@@ -16,15 +17,44 @@ def test_cp():
     tempd = tempfile.mkdtemp()
     try:
         a = os.path.join(tempd, 'a')
+        a1 = os.path.join(a, 'a1')
+        b1 = os.path.join(a, 'b', 'b1')
+
         e3.fs.mkdir(a)
-        e3.fs.echo_to_file(os.path.join(a, 'a1'), 'a1')
+        e3.fs.echo_to_file(a1, 'a1')
         e3.fs.mkdir(os.path.join(a, 'b'))
-        e3.fs.echo_to_file(os.path.join(a, 'b', 'b1'), 'b1')
+        e3.fs.echo_to_file(b1, 'b1')
+
         dest = os.path.join(tempd, 'dest')
         e3.fs.mkdir(dest)
         e3.fs.cp(a, dest, recursive=True)
-        assert os.path.exists(os.path.join(dest, 'a', 'b', 'b1'))
         assert os.path.exists(os.path.join(dest, 'a', 'a1'))
+        assert os.path.exists(os.path.join(dest, 'a', 'b', 'b1'))
+
+        dest2 = os.path.join(tempd, 'dest2')
+
+        with pytest.raises(e3.fs.FSError) as err:
+            e3.fs.cp('*.non_existing', dest2)
+            assert "can't find files matching" in err
+
+        with pytest.raises(e3.fs.FSError) as err:
+            e3.fs.cp([a1, b1], dest2)
+            assert 'target should be a directory' in err
+
+        e3.fs.mkdir(dest2)
+        e3.fs.cp([a1, b1], dest2)
+        assert os.path.exists(os.path.join(dest2, 'a1'))
+        assert os.path.exists(os.path.join(dest2, 'b1'))
+
+        dest3 = os.path.join(tempd, 'dest3')
+        e3.fs.mkdir(dest3)
+        e3.fs.cp(a, dest3, copy_attrs=False, recursive=True)
+        e3.fs.cp(a1, dest3, copy_attrs=False)
+
+        assert os.path.exists(os.path.join(dest3, 'a', 'a1'))
+        assert os.path.exists(os.path.join(dest3, 'a', 'b', 'b1'))
+        assert os.path.exists(os.path.join(dest3, 'a1'))
+
     finally:
         e3.fs.rm(tempd, True)
 
@@ -38,17 +68,54 @@ def test_echo():
         e3.fs.echo_to_file(tf.name, 'bar', append=True)
         with open(tf.name) as foobar_f:
             assert foobar_f.read() == 'foobar'
+
+        e3.fs.echo_to_file(tf.name, ['line1', 'line2'])
+        with open(tf.name) as fd:
+            assert fd.read().strip() == 'line1\nline2'
     finally:
         e3.fs.rm(tf.name)
 
 
 def test_find():
-    assert os.path.abspath(__file__) in {
-        os.path.abspath(f) for f in e3.fs.find(os.path.dirname(__file__))}
+    d = os.path.dirname(__file__)
+    parent_d = os.path.dirname(d)
+
+    result = {os.path.abspath(f) for f in e3.fs.find(parent_d)}
+    assert os.path.abspath(__file__) in result
+    assert os.path.abspath(d) not in result
+
+    result = e3.fs.find(parent_d, include_dirs=True, include_files=False)
+    result = {os.path.abspath(f) for f in result}
+    assert os.path.abspath(d) in result
+    assert os.path.abspath(__file__) not in result
+
+    result = e3.fs.find(parent_d, include_dirs=True, include_files=True)
+    result = {os.path.abspath(f) for f in result}
+    assert os.path.abspath(d) in result
+    assert os.path.abspath(__file__) in result
 
 
 def test_tree_state():
-    # Just check that get_filetree_state returns a string
-    assert isinstance(
-        e3.fs.get_filetree_state(os.path.dirname(__file__)),
-        str)
+    import time
+    tempd = tempfile.mkdtemp()
+    try:
+        d = os.path.dirname(os.path.dirname(__file__))
+        state = e3.fs.get_filetree_state(d)
+        assert isinstance(state, str)
+
+        e3.fs.sync_tree(d, os.path.join(tempd))
+        state2 = e3.fs.get_filetree_state(unicode(tempd))
+        assert state != state2
+
+        state3 = e3.fs.get_filetree_state(tempd)
+        assert state2 == state3
+
+        # To ensure that file system resolution is not hidding
+        # changes
+        time.sleep(2)
+
+        e3.os.fs.touch(os.path.join(tempd, 'toto'))
+        state4 = e3.fs.get_filetree_state(tempd)
+        assert state4 != state3
+    finally:
+        e3.fs.rm(tempd, True)
