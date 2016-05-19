941263697e46e7b109f435533c177517420d42e7
Olivier Ramonat <ramonat@adacore.com>
2016-05-19 17:09:18 +0200

    Add test for e3.log
    Change-Id: I6136dd8747f0c9d29c13f79874653728897a55bd
    TN: P510-063

---
+++ tests/tests_e3/log/main_test.py
@@ -0,0 +1,26 @@
+import dateutil.parser
+import datetime
+import sys
+
+import e3.log
+import e3.os.process
+
+
+def test_log():
+    p = e3.os.process.Run(
+        [sys.executable, '-c',
+         '\n'.join((
+             'import e3.log',
+             'e3.log.activate(filename="log.txt")',
+             'l = e3.log.getLogger("test_log")',
+             'l.debug("this is a log record")'))])
+    assert p.status == 0
+
+    with open('log.txt') as f:
+        line = f.readline()
+        # Get datetime in the log
+        log_datetime, _, _ = line.partition(': ')
+
+        # Parse it and verify that it is it in GMT
+        assert (datetime.datetime.utcnow() -
+                dateutil.parser.parse(log_datetime)).seconds < 10
