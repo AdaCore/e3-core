0fbf732ff8c50834338ae6e49d5d03a2794c9562
Olivier Ramonat <ramonat@adacore.com>
2016-07-19 18:51:07 +0200

    Add a simple event framework
    Add two main classes Event and EventManager that will need to
    be subclassed.
    Also add a function load_event_manager() to load an EventManager
    from an entry point:
    
    To implement an EventManager, first extend the Event and
    EventManager classes::
    
        class MyEvent(Event):
            ...
    
        class MyEventManager(EventManager):
            Event = MyEvent
    
            def send_event(self, event):
               ...
    
    Then add an entry point in your setup.py::
    
        entry_points={
            'e3.event.manager': [
               'mymanager = mymanager:MyEventManager']}
    
    Then you will be able to load it via::
    
        manager = e3.event.load_event_manager(
            name='mymanager',
            configuration={...})
    
    And send your first event::
    
        with contextlib.closing(manager.Event(...)) as e:
            e.attach_file(path=...)
        manager.send_event(e)
    
    Change-Id: I8ce86ff71be6fac8216e005584e376250b1c1735

---
+++ e3/event/__init__.py
@@ -0,0 +1,9 @@
+import stevedore
+
+
+def load_event_manager(name, configuration):
+    plugin = stevedore.DriverManager(
+        'e3.event.manager',
+        name)
+
+    return plugin.driver(configuration)
