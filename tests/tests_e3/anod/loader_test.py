ce85d597601f96a0223614a3bb63fb0cf6311612
Olivier Ramonat <ramonat@adacore.com>
2016-09-08 23:27:54 +0200

    Use AnodSpecRepository, fix anod tests
    Change-Id: If936f032dc6719117cd3c70270e9e549aced490c

--- tests/tests_e3/anod/loader_test.py
+++ tests/tests_e3/anod/loader_test.py
@@ -1,12 +1,9 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import e3.anod.loader
-from e3.anod.loader import spec
+from e3.anod.loader import AnodSpecRepository
 from e3.anod.error import SandBoxError
-from e3.anod.sandbox import SandBox
 
-import e3.fs
 
 import os
 import pytest
@@ -15,30 +12,29 @@ import pytest
 class TestLoader(object):
 
     def test_spec_does_not_exist(self):
-        e3.anod.loader.sandbox = SandBox()
-        e3.anod.loader.sandbox.spec_dir = '/foo/bar'
         with pytest.raises(SandBoxError) as err:
-            spec('/does/not/exist')
+            AnodSpecRepository('/foo/bar')
+
         assert str(err.value).startswith(
-            'load: the spec /does/not/exist.anod does not exist')
+            'spec directory /foo/bar does not exist')
 
     def test_spec_loader1(self):
-        e3.anod.loader.sandbox = SandBox()
-        e3.anod.loader.sandbox.spec_dir = os.path.join(
+        spec_dir = os.path.join(
             os.path.dirname(__file__),
             'data')
 
-        s = spec('loader1')
+        spec_repo = AnodSpecRepository(spec_dir)
+        s = spec_repo.load('loader1')
         assert s.name == 'loader1'
-        assert s.sandbox == e3.anod.loader.sandbox
 
     def test_spec_loader2(self):
-        e3.anod.loader.sandbox = SandBox()
-        e3.anod.loader.sandbox.spec_dir = os.path.join(
+        spec_dir = os.path.join(
             os.path.dirname(__file__),
             'data')
 
+        spec_repo = AnodSpecRepository(spec_dir)
+
         with pytest.raises(SandBoxError) as err:
-            spec('loader2')
+            spec_repo.load('loader2')
         assert str(err.value).startswith(
             'load: cannot find Anod subclass in')
