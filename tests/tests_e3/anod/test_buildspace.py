db55665e428517d3512c66843811fc0b870950a5
Olivier Ramonat <ramonat@adacore.com>
2016-10-09 15:47:03 +0200

    Add fingerprint + status tests
    Change-Id: I90ced92ea2dbfe938471628969bf59ca66874bde

--- tests/tests_e3/anod/test_buildspace.py
+++ tests/tests_e3/anod/test_buildspace.py
@@ -1,6 +1,9 @@
+from e3.anod.status import ReturnValue
 from e3.anod.buildspace import BuildSpace
+from e3.anod.fingerprint import Fingerprint
 from e3.os.fs import touch
 
+import datetime
 import os
 import pytest
 
@@ -28,3 +31,40 @@ def test_subdir():
     bs = BuildSpace(root_dir=os.getcwd(), primitive='build')
     with pytest.raises(ValueError):
         bs.get_subdir('foo')
+
+
+def test_fingerprint():
+    """Check dump/restore of fingerprint."""
+    bs = BuildSpace(root_dir=os.getcwd(), primitive='build')
+    bs.create()
+    fp = Fingerprint()
+    fp.add('foo', 'bar')
+    bs.save_fingerprint(kind='build', fingerprint=fp)
+
+    # Check that the fingerprint stored on disk is equal to fp
+    fp_checksum = bs.load_fingerprint(kind='build', sha1_only=True)
+    assert isinstance(fp_checksum, str)
+
+    loaded_fp = bs.load_fingerprint(kind='build')
+    assert loaded_fp == fp
+
+    # Now make sure that load_fingerprint does not fail when the bumped
+    # value is corrupted
+    with open(os.path.join(bs.meta_dir, 'build_fingerprint.yaml'), 'w') as f:
+        f.write('[')
+
+    assert bs.load_fingerprint(kind='build') == Fingerprint()
+
+
+def test_status():
+    """Check dump/restore of status."""
+    bs = BuildSpace(root_dir=os.getcwd(), primitive='build')
+    bs.create()
+    bs.save_last_status(kind='test', status=ReturnValue.notready)
+
+    last_status, timestamp = bs.get_last_status(kind='test')
+    assert last_status == ReturnValue.notready
+    assert (datetime.datetime.now() - timestamp).total_seconds() < 5
+    last_status, timestamp = bs.get_last_status(kind='install')
+    assert last_status == ReturnValue.missing
+    assert timestamp is None
