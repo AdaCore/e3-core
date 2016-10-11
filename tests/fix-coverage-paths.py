68053bb520ef5ff37d962f59c0e9699395ecfed0
Olivier Ramonat <ramonat@adacore.com>
2016-10-11 17:03:15 +0200

    Rework coverage script to report nicer results
    Hack .coverage in order to get good looking paths, we want to
    report the path starting with e3 and not exactly what we're
    testing (which is .tox/*/lib/python*/site-packages/e3 on UNIX).
    
    Generate a .coveragerc file so that calls to coverage report
    works out of the box.
    
    Change-Id: I342f0667358f6ff165e8ecff2b3f5ad8cf790945

---
+++ tests/fix-coverage-paths.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# strip .tox/*/lib/python*/site-packages paths from coverage data
+# show only paths corresponding to the real source files
+# From https://github.com/danilobellini/pytest-doctest-custom/
+
+from coverage.data import PathAliases, CoverageData
+import os
+import sys
+
+
+def fix_paths(site_pkg_dir, cov_data_file):
+    site_pkg_dir = os.path.abspath(site_pkg_dir)
+
+    paths = PathAliases()
+    paths.add(site_pkg_dir, '.')
+
+    old_coverage_data = CoverageData()
+    old_coverage_data.read_file(cov_data_file)
+
+    new_coverage_data = CoverageData()
+    new_coverage_data.update(old_coverage_data, paths)
+
+    new_coverage_data.write_file(cov_data_file)
+
+
+if __name__ == '__main__':
+    fix_paths(sys.argv[1], sys.argv[2])
