3e8f23393ea13dd607638f3363bdf471aba0acf5
Olivier Ramonat <ramonat@adacore.com>
2016-10-13 22:59:29 +0200

    Full line coverage for e3.diff
    Change-Id: Ie30efe383f6ffb84cc0c78a2419aee18567763c2

--- tests/tests_e3/diff/main_test.py
+++ tests/tests_e3/diff/main_test.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import e3.diff
 import e3.fs
 import os
+import pytest
 
 
 def test_non_existing():
@@ -28,10 +29,57 @@ def test_patch():
     with open(file_after_patch) as fd:
         expected = fd.readlines()
 
-    assert e3.diff.diff(expected, output) == ''
+    # By default empty line, leading and trailing whitespaces are ignored
+    assert e3.diff.diff(expected + [''], output) == ''
+    assert e3.diff.diff([' ' + l + ' ' for l in expected], output) == ''
+    assert e3.diff.diff(expected, output, ignore_white_chars=False) == ''
+
+    # we can also ignore specific pattern
+    assert '-to ignore' in e3.diff.diff(expected + ['to ignore'], output)
+    assert e3.diff.diff(expected + ['to ignore'], output,
+                        ignore='ig.*re') == ''
 
     e3.fs.cp(file_patch2, current_dir)
     e3.diff.patch('patch2.txt', current_dir,
-                  discarded_files=['dummy_to_patch.new.txt'])
+                  discarded_files=[u'dummy_to_patch.new.txt'])
     assert e3.diff.diff('file_to_patch.orig.txt',
                         file_after_patch2) == ''
+
+    e3.fs.cp(file_patch2, current_dir)
+    e3.diff.patch(
+        'patch2.txt', current_dir,
+        discarded_files=lambda x: x.endswith(b'new.txt'))
+
+
+def test_patch_ignore_all(caplog):
+    test_dir = os.path.dirname(__file__)
+    file_to_patch = os.path.join(test_dir, 'file_to_patch.orig.txt')
+    file_patch2 = os.path.join(test_dir, 'patch2.txt')
+
+    current_dir = os.getcwd()
+
+    e3.fs.cp(file_to_patch, current_dir)
+    e3.fs.cp(file_patch2, current_dir)
+    e3.diff.patch(
+        'patch2.txt', current_dir,
+        discarded_files=lambda x: True)
+
+    assert 'All patch2.txt content has been discarded' in caplog.text()
+
+
+def test_patch_invalid():
+    test_dir = os.path.dirname(__file__)
+    file_to_patch = os.path.join(test_dir, 'file_to_patch.orig.txt')
+    file_patch2 = os.path.join(test_dir, 'patch2.txt')
+
+    current_dir = os.getcwd()
+
+    e3.fs.cp(file_to_patch, current_dir)
+    e3.fs.cp(file_patch2, current_dir)
+
+    with open('patch2.txt', 'a') as f:
+        f.write('invalid')
+
+    with pytest.raises(e3.diff.DiffError):
+        e3.diff.patch(
+            'patch2.txt', current_dir)
