0886a169f842b976db15e5a6b13126a2cda96644
Olivier Ramonat <ramonat@adacore.com>
2016-10-13 22:12:21 +0200

    Full line coverage for e3.decorator
    Change-Id: Id1a8eecf1c95e034cb87fc115282ee5069ba5a0f

--- tests/tests_e3/decorator/main_test.py
+++ tests/tests_e3/decorator/main_test.py
@@ -1,17 +1,49 @@
 from __future__ import absolute_import
 from random import random
+
+import pytest
+
 import e3.decorator
 
 
 def test_memoize():
+    # First generate a function returning random values so that we
+    # can see whether the cache is used or not
 
     @e3.decorator.memoize
     def t(arg):
+        """Do foo."""
         del arg
         return random()
 
+    assert 'Do foo.' in t.__repr__()
+
     assert t(1) == t(1)
     assert t(1) != t(2)
+
+    # kwargs not supported
+    with pytest.raises(TypeError):
+        assert t(arg=1)
+
+    # cache not used when non-hashable arguments
+    assert t([1, 2, 3]) != t([1, 2, 3])
+
+    # Verify that the cache is working also when
+    # calling instance methods
+
+    class C(object):
+
+        @e3.decorator.memoize
+        def t(self, arg):
+            del arg
+            return random()
+
+    # same instance use the same cache
+    c_instance = C()
+    assert c_instance.t(2) == c_instance.t(2)
+
+    # but not different instances
+    assert C().t(2) != C().t(2)
 
 
 def test_enabled():
