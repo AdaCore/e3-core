09e0c30deec585098d7b65e5877b391fb4499ecf
Olivier Ramonat <ramonat@adacore.com>
2016-10-18 13:01:05 +0200

    Full coverage of e3.hash
    Change-Id: I7b31d590a0b8bb346ef6605df740a605f1f6a5ed

---
+++ tests/tests_e3/hash/main_test.py
@@ -0,0 +1,13 @@
+import pytest
+
+import e3.hash
+
+
+def test_hash():
+    with open('to-hash.txt', 'wb') as f:
+        f.write(b'content\n')
+    assert e3.hash.md5(f.name) == 'f75b8179e4bbe7e2b4a074dcef62de95'
+    assert e3.hash.sha1(f.name) == '7fe70820e08a1aac0ef224d9c66ab66831cc4ab1'
+
+    with pytest.raises(e3.hash.HashError):
+        e3.hash.md5('doesnotexist')
