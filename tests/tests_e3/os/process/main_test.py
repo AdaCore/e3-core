cd2d4357a97bd77463555d05c418215a4a4fe8b8
Olivier Ramonat <ramonat@adacore.com>
2016-11-14 21:35:43 +0100

    Rewrite the wait_for_processes test to avoid raise condition
    Don't use time.sleep() but wait for specific event (creation of
    files) in order to stop the processes.
    
    Also set a timeout when calling wait_for_processes.

--- tests/tests_e3/os/process/main_test.py
+++ tests/tests_e3/os/process/main_test.py
@@ -60,17 +60,22 @@ def test_enable_commands_handler():
 @pytest.mark.xfailif(sys.platform != 'win32',
                      reason="unix implem not complete")
 def test_wait_for_processes():
-    p1 = e3.os.process.Run([sys.executable, '-c',
-                            'import time; time.sleep(3); print("process1")'],
-                           bg=True)
-    p2 = e3.os.process.Run([sys.executable, '-c',
-                            'import time; time.sleep(4); print("process2")'],
-                           bg=True)
+    for v in (1, 2):
+        with open('p%d.py' % v, 'w') as f:
+            f.write('import os\n'
+                    'while True:\n'
+                    '    if os.path.exists("end%d"): break\n'
+                    'print("process%d")\n' % (v, v))
+
+    p1 = e3.os.process.Run([sys.executable, 'p1.py'], bg=True)
+    p2 = e3.os.process.Run([sys.executable, 'p2.py'], bg=True)
 
     process_list = [p1, p2]
-    result = e3.os.process.wait_for_processes(process_list, 0)
+    e3.os.fs.touch('end1')
+    result = e3.os.process.wait_for_processes(process_list, 10)
     del process_list[result]
-    e3.os.process.wait_for_processes(process_list, 0)
+    e3.os.fs.touch('end2')
+    e3.os.process.wait_for_processes(process_list, 10)
 
     assert p1.status == 0
     assert p1.out.strip() == 'process1'
