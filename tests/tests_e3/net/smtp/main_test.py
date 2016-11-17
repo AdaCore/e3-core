1e4325c1bf47ad0d3693cf8337c4c2ee98b84388
SayfEddine <hammemi@adacore.com>
2016-11-17 10:07:25 +0100

    Increase coverage of e3.fs and e3.net.smtp

--- tests/tests_e3/net/smtp/main_test.py
+++ tests/tests_e3/net/smtp/main_test.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 import mock
 import e3.net.smtp
-
+import smtplib
 
 def test_sendmail():
     from_addr = 'e3@example.net'
@@ -21,3 +21,45 @@ def test_sendmail():
             from_addr,
             to_addresses,
             msg_as_string)
+
+def test_sendmail_onerror():
+    from_addr = 'e3@example.net'
+    to_addresses = ['info@example.net', 'info@example.com']
+    msg_as_string = 'test mail content'
+    msg_size_exceed = "A"*1200
+    result = e3.net.smtp.sendmail(
+        from_addr,
+        to_addresses,
+        msg_size_exceed,
+        ['smtp.localhost'],
+        max_size=8/1024)
+    assert result is False
+
+    with mock.patch('smtplib.SMTP') as mock_smtp:
+        mock_smtp.side_effect = smtplib.SMTPException()
+        result = e3.net.smtp.sendmail(
+            from_addr,
+            to_addresses,
+            msg_as_string,
+            ['smtp.localhost'])
+        assert result is False
+
+    with mock.patch('smtplib.SMTP') as mock_smtp:
+        smtp_mock = mock_smtp.return_value
+        smtp_mock.sendmail.side_effect = smtplib.SMTPException()
+        result = e3.net.smtp.sendmail(
+            from_addr,
+            to_addresses,
+            msg_as_string,
+            ['smtp.localhost'])
+        assert result is False
+
+    with mock.patch('smtplib.SMTP') as mock_smtp:
+        smtp_mock = mock_smtp.return_value
+        smtp_mock.sendmail.return_value = {}
+        result = e3.net.smtp.sendmail(
+            from_addr,
+            to_addresses,
+            msg_as_string,
+            ['smtp.localhost'])
+        assert result is True
