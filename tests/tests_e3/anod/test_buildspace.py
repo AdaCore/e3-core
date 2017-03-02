8b296fd4ebea1dcbf08fd25ae98fd2e25eb37200
Olivier Ramonat <ramonat@adacore.com>
2017-03-02 13:39:51 -0500

    Full statement coverage of e3.anod.buildspace

--- tests/tests_e3/anod/test_buildspace.py
+++ tests/tests_e3/anod/test_buildspace.py
@@ -2,8 +2,10 @@ from __future__ import absolute_import, division, print_function
 
 import datetime
 import os
+import time
 
 from e3.anod.buildspace import BuildSpace
+from e3.anod.error import AnodError
 from e3.anod.fingerprint import Fingerprint
 from e3.anod.status import ReturnValue
 from e3.os.fs import touch
@@ -58,6 +60,27 @@ def test_fingerprint():
 
     assert bs.load_fingerprint(kind='build') == Fingerprint()
 
+    # Try updating the fingerprint
+    fp.add('key', 'value')
+    bs.update_status(
+        kind='build', status=ReturnValue.notready,
+        fingerprint=fp)
+    assert bs.get_last_status(kind='build')[0] == ReturnValue.notready
+    loaded_fp = bs.load_fingerprint(kind='build')
+    assert loaded_fp == fp
+
+    # Updating build fingerprint also update install fingerprint
+    loaded_fp = bs.load_fingerprint(kind='install')
+    assert loaded_fp == fp
+
+    # Now update install status
+
+    bs.update_status(kind='install', status=ReturnValue.success)
+    assert bs.get_last_status(kind='install')[0] == ReturnValue.success
+
+    # build status should not be modified
+    assert bs.get_last_status(kind='build')[0] == ReturnValue.notready
+
 
 def test_status():
     """Check dump/restore of status."""
@@ -82,3 +105,33 @@ def test_logs():
     assert os.path.isfile(bs.log_file)
     with open(bs.log_file) as f:
         assert 'anod primitive: build' in f.read()
+
+
+def test_live_logs():
+    bs = BuildSpace(root_dir=os.getcwd(), primitive='build')
+    bs.create()
+    bs.set_logging(stdout_logs=True)
+    bs.log_stream.write('here')
+    bs.log_stream.flush()
+    bs.log_stream.write('there')
+    time.sleep(0.2)  # let enough time to cover the tail_thread code
+    bs.end()
+    with open(bs.log_file) as f:
+        assert 'here' in f.read()
+
+
+def test_dump_traceback():
+    bs = BuildSpace(root_dir=os.getcwd(), primitive='build')
+    bs.create()
+    try:
+        raise ValueError('dumping traceback now')
+    except ValueError:
+        with pytest.raises(AnodError) as anod_err:
+            bs.dump_traceback('foo', 'test')
+
+    assert 'dumping traceback now' in str(anod_err)
+
+    trace_file = os.path.join(bs.log_dir, 'traceback_test')
+    assert os.path.exists(trace_file)
+    with open(trace_file) as f:
+        assert 'dumping traceback now' in f.read()
