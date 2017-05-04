b758ecda4e8a1b560bc1cdbb5fe9682518b659ba
Olivier Ramonat <ramonat@adacore.com>
2017-05-04 14:10:11 +0200

    Minor test refactoring

--- tests/tests_e3/electrolyt/plan/main_test.py
+++ tests/tests_e3/electrolyt/plan/main_test.py
@@ -4,27 +4,38 @@ import e3.electrolyt.host as host
 import e3.electrolyt.plan as plan
 
 
-def test_simple_plan():
-
-    server = host.Host(hostname='myserver',
+def _get_new_plancontext(machine_name):
+    server = host.Host(hostname=machine_name,
                        platform='x86_64-linux',
                        version='suse11')
-    context = plan.PlanContext(server=server)
 
     def build(spec, build=None, host=None, target=None):
         return 'build ' + spec
 
-    myplan = plan.Plan(data={'a': 'a', 'b': 'b'})
-
-    with open('plan.txt', 'w') as f:
-        f.write(u'def myserver():\n')
-        f.write(u'    build(a)\n')
-        f.write(u'    build(b, build="x86-linux", host="x86-linux",')
-        f.write(u'          target="x86-windows")\n')
-
-    myplan.load('plan.txt')
-
+    context = plan.PlanContext(server=server)
     context.register_action('build', build)
+    return context
+
+
+def _get_plan(data, content):
+    myplan = plan.Plan(data)
+    with open('plan.txt', 'w') as f:
+        f.write('\n'.join(content))
+    myplan.load('plan.txt')
+    return myplan
+
+
+def test_simple_plan():
+    """Test a very basic electrolyt plan."""
+    context = _get_new_plancontext('myserver')
+
+    myplan = _get_plan(
+        data={'a': 'a', 'b': 'b'},
+        content=[u'def myserver():\n',
+                 u'    build(a)\n',
+                 u'    build(b, build="x86-linux", host="x86-linux",',
+                 u'          target="x86-windows")\n'])
+
     actions = context.execute(myplan, 'myserver')
     assert len(actions) == 2
     assert actions[0].spec == 'a'
