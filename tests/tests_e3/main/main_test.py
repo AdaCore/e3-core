b391d7ae7594fb3ed89ce6c579b464ac56ce62fe
Olivier Ramonat <ramonat@adacore.com>
2017-06-15 21:38:48 +0200

    Test SIGTERM handler in Main()

--- tests/tests_e3/main/main_test.py
+++ tests/tests_e3/main/main_test.py
@@ -1,9 +1,44 @@
 from __future__ import absolute_import, division, print_function
 
+import sys
+
 import e3.env
 import e3.os.process
+
+import pytest
 
 
 def test_main():
     assert e3.env.Env().build.platform in \
         e3.os.process.Run(['e3', '--platform-info=build']).out
+
+
+def test_mainprog():
+    with open('mymain.py', 'w') as f:
+        f.write('\n'.join((
+            '#!/usr/bin/env python',
+            'from e3.main import Main',
+            'import os',
+            'm = Main(name="testmain")',
+            'm.parse_args()',
+            'm.argument_parser.print_usage()')))
+    p = e3.os.process.Run([sys.executable, 'mymain.py', '--nocolor'])
+    assert 'mymain' in p.out
+
+
+@pytest.mark.skipif(sys.platform == 'win32',
+                    reason='Signal handler not set on windows')
+def test_sigterm():
+    with open('mymain.py', 'w') as f:
+        f.write('\n'.join((
+            '#!/usr/bin/env python',
+            'from e3.main import Main',
+            'import os',
+            'import signal',
+            'm = Main()',
+            'm.parse_args()',
+            'import time',
+            'time.sleep(10)')))
+    p = e3.os.process.Run([sys.executable, 'mymain.py', '--nocolor'],
+                          timeout=1)
+    assert 'SIGTERM received' in p.out
