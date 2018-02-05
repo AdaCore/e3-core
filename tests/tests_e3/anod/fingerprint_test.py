918dbf7ea39fdc0889fd79e3805bd672938d57ff
Joel Brobecker <brobecker@adacore.com>
2018-02-05 15:40:17 +0400

    change Fingerprint.elements to a simple dict rather than an OrderedDict
    There is no need for the elements of a fingerprint to be stored in
    an OrderedDict, so this patch changes the implementation to use
    a dictionary instead, taking care to sure that the behavior remains
    independent of the order the elements are being added. Additional
    testing is added to verify this property.
    
    This will also simplify the implementation of methods for saving
    and restoring fingerprints to/from files.
    
    TN: QB17-039

--- tests/tests_e3/anod/fingerprint_test.py
+++ tests/tests_e3/anod/fingerprint_test.py
@@ -47,6 +47,52 @@ def test_fingerprint():
     assert len(f6.sha1()) == 40
 
 
+def test_add_order_not_important():
+    def create_fingerprint(first, second, third):
+        """Create a fingerprint with 3 elements added in the given order.
+
+        The arguments, first, second, and third should be either
+        1, 2, or 3 (integers), and are meant as indices to (key, value)
+        pairs in the idx_to_entry_map below, indicating which part
+        of the fingerprint gets added in what order.
+
+        :rtype: Fingerprint
+        """
+        idx_to_entry_map = {1: ('foo', '1'),
+                            2: ('bar', '2'),
+                            3: ('baz', '3')}
+        f = Fingerprint()
+        f.add(*idx_to_entry_map[first])
+        f.add(*idx_to_entry_map[second])
+        f.add(*idx_to_entry_map[third])
+        return f
+
+    f_ref = create_fingerprint(1, 2, 3)
+
+    def check_scenario(first, second, third):
+        """Check scenario where adding fingerprint elements in given order.
+
+        Create a fingerprint using create_fingerprint above
+        (same arguments), and then check the resulting key against
+        the reference key f_ref above.  Both the reference fingerprint
+        and the new fingerprint have the same contents, so they should
+        behave the same in all ways.
+        """
+        f = create_fingerprint(first, second, third)
+        assert f == f_ref
+        assert f.compare_to(f_ref) is None
+        assert f_ref.compare_to(f) is None
+        assert str(f) == str(f_ref)
+        assert f.sha1() == f_ref.sha1()
+
+    check_scenario(1, 2, 3)
+    check_scenario(1, 3, 2)
+    check_scenario(2, 1, 3)
+    check_scenario(2, 3, 1)
+    check_scenario(3, 1, 2)
+    check_scenario(3, 2, 1)
+
+
 def test_fingerprint_version():
     """Changing the FINGERPRINT_VERSION modify the fingerprint sha1."""
     import e3.anod.fingerprint
