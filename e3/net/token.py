62b7e9b9f728e9ea632acd7d74a0d6dc380a6f97
Anna Kaczorowski <kaczor@adacore.com>
2018-02-20 11:01:12 +0100

    Add read and verify (JW)Token
    TN:R115-030

---
+++ e3/net/token.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import, division, print_function
+
+import base64
+import calendar
+import json
+from datetime import datetime
+
+
+def get_payload(token):
+    """Get payload from a JSON Web Token.
+
+    :param token: token
+    :type token: str
+    :return: decoded payload
+    :rtype: dict
+    """
+    # Extract the payload
+    signing_part, _ = token.rsplit('.', 1)
+    _, payload = signing_part.split('.', 1)
+
+    # Add required padding
+    rem = len(payload) % 4
+    if rem > 0:
+        payload += '=' * (4 - rem)
+    return json.loads(base64.b64decode(payload).decode('utf-8'))
+
+
+def utc_timestamp():
+    """Return the number of seconds since epoch UTC.
+
+    :return: the number of seconds since epoch UTC
+    :rtype: int
+    """
+    d = datetime.utcnow()
+    return calendar.timegm(d.utctimetuple())
+
+
+def is_valid(token):
+    """Return true if the token is still valid, false otherwise.
+
+    :param token: full token
+    :type token: str
+    :return: return true if the token is still valid, false otherwise
+    :rtype: bool
+    """
+    payload = get_payload(token)
+    return payload.get('typ') == 'Bearer' and \
+        payload.get('exp', 0) > utc_timestamp()
---
+++ tests/tests_e3/net/token/main_test.py
@@ -0,0 +1,78 @@
+from __future__ import absolute_import, division, print_function
+
+import base64
+import hashlib
+import json
+
+from e3.net.token import get_payload, is_valid
+
+FUTURE_TIMESTAMP = 9999999999999
+
+
+def create_token(payload):
+    """Return JSON Web Token.
+
+    :param payload:
+    :type payload: dict
+    :return: token
+    :rtype: str
+    """
+    payload_bytes = json.dumps(payload).encode('utf-8')
+    data = b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9' + b'.'\
+           + base64.b64encode(payload_bytes)
+    signature = hashlib.sha256(data).hexdigest().encode('utf-8')
+    token = data + b'.' + signature
+    return token.decode('utf-8')
+
+
+def test_get_payload():
+    expected = {u'GivenName': u'Toto',
+                u'Email': u'toto@example.com',
+                u'Role': u'tester',
+                u'iss': u'adacore.com',
+                u'sub': u'test',
+                u'exp': 1550655711,
+                u'iat': 1519119711,
+                u'Surname': u'123',
+                u'aud': u'www.adacore.com'}
+    token = create_token(expected)
+    payload = get_payload(token)
+    assert payload == expected
+
+
+def test_valid_token():
+    valid_token = create_token(
+        {u'typ': u'Bearer', u'exp': FUTURE_TIMESTAMP})
+    assert is_valid(valid_token)
+
+
+def test_wrong_token_type():
+    badtype_token = create_token(
+        {u'typ': u'badtype', u'exp': FUTURE_TIMESTAMP})
+    assert not is_valid(badtype_token)
+
+
+def test_old_token():
+    old_token = create_token(
+        {u'typ': u'Bearer', u'exp': 1419064452})
+    assert not is_valid(old_token)
+
+
+def test_token_str():
+    token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZGFjb3JlLmNvbSI'\
+            'sImlhdCI6MTUxOTExOTcxMSwiZXhwIjoxNTUwNjU1NzExLCJhdWQiOiJ3d3cuYWR'\
+            'hY29yZS5jb20iLCJzdWIiOiJ0ZXN0IiwiR2l2ZW5OYW1lIjoiVG90byIsIlN1cm5'\
+            'hbWUiOiIxMjMiLCJFbWFpbCI6InRvdG9AZXhhbXBsZS5jb20iLCJSb2xlIjoidGV'\
+            'zdGVyIn0.UERDJuUFh6A30TXIEKbNu6BbDOUCdsLGMKuQS8DfboA'
+    payload = get_payload(token)
+    expected = {u'GivenName': u'Toto',
+                u'Email': u'toto@example.com',
+                u'Role': u'tester',
+                u'iss': u'adacore.com',
+                u'sub': u'test',
+                u'exp': 1550655711,
+                u'iat': 1519119711,
+                u'Surname': u'123',
+                u'aud': u'www.adacore.com'}
+    assert payload == expected
+    assert not is_valid(token)
