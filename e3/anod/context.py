8104290716fdc1301619555972a18d8693ec05df
Olivier Ramonat <ramonat@adacore.com>
2019-06-17 10:02:49 +0200

    Fix explicit inclusion of source packages
    When creating a CreateSources node with a subset of source packages
    the parameter source_packages contain a list of source names and
    not SourceBuilder objects.
    
    TN: S613-023

--- e3/anod/context.py
+++ e3/anod/context.py
@@ -360,7 +360,7 @@ class AnodContext(object):
 
                 # Then one node for each source package
                 for sb in spec.source_pkg_build:
-                    if source_packages and sb not in source_packages:
+                    if source_packages and sb.name not in source_packages:
                         # This source package is defined in the spec but
                         # explicitly excluded in the plan
                         continue
