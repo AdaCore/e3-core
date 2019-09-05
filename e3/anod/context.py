45824fec861bf7471fec417572e2682cad31fd6b
Olivier Ramonat <ramonat@adacore.com>
2019-09-05 19:16:17 +0200

    Revert "Don't raise an error if the same deps is added several time"
    This reverts commit 266a4cb4fbd8ecf8f7c5dd717413b5bdc7b97b0d.
    This is not needed anymore now that we create a separate instance for
    each source package creation.
    TN: S905-021

--- e3/anod/context.py
+++ e3/anod/context.py
@@ -378,8 +378,7 @@ class AnodContext(object):
             :param dep_instance: the Anod instance loaded for that dependency
             :type dep_instance: Anod
             """
-            if dep.local_name in spec_instance.deps and \
-                    (spec_instance.deps[dep.local_name] != dep_instance):
+            if dep.local_name in spec_instance.deps:
                 raise AnodError(
                     origin='expand_spec',
                     message='The spec {} has two dependencies with the same '
