f0402d2b39384ff0a55b71e5aadfea0f21993eb4
Frederic Leger <leger@adacore.com>
2023-11-21 15:52:49 +0100

    Added a missing package class.
    - Modified the Document.add_package() method to take a package for input,
      not many parameters which are used to build a package in the end.
    - Updated the tests for full coverage of the SPDX module, and to take
      into account the modifications of the add_package() method.
    - Added more documentation for Package class fields.
    - Added package comments to the package class.
    - Added links to the specification for the package fields.

--- tests/tests_e3/spdx_test.py
+++ tests/tests_e3/spdx_test.py
@@ -1,15 +1,29 @@
 from e3.spdx import (
     Document,
+    EntityRef,
     ExternalRef,
     ExternalRefCategory,
+    FilesAnalyzed,
     Creator,
     Organization,
     Tool,
+    Package,
+    PackageComment,
+    PackageCopyrightText,
+    PackageDownloadLocation,
+    PackageFileName,
+    PackageLicenseComments,
+    PackageLicenseConcluded,
+    PackageLicenseDeclared,
+    PackageName,
     PackageOriginator,
     PackageSupplier,
+    PackageVersion,
     Person,
     SHA1,
     SHA256,
+    SPDXID,
+    SPDXEntryMaybeStrMultilines,
     NOASSERTION,
     Relationship,
     RelationshipType,
@@ -22,6 +36,9 @@ import pytest
 def test_entities_ref_spdx():
     org = Organization("AdaCore")
     assert org.to_tagvalue() == "Organization: AdaCore"
+    assert "AdaCore" in str(org)
+    assert "NOASSERTION" in str(Organization(NOASSERTION))
+    assert Organization(NOASSERTION).to_json_dict() == {"organization": "NOASSERTION"}
 
     assert Creator(org).to_tagvalue() == "Creator: Organization: AdaCore"
 
@@ -31,6 +48,19 @@ def test_entities_ref_spdx():
     assert (
         PackageOriginator(NOASSERTION).to_tagvalue() == "PackageOriginator: NOASSERTION"
     )
+
+
+def test_entity_ref() -> None:
+    """Tests for the EntiryRef class which are not covered by the other tests."""
+    org: EntityRef = EntityRef(Organization("AdaCore"))
+    no_assertion: EntityRef = EntityRef(NOASSERTION)
+
+    assert org.to_tagvalue() == "EntityRef: Organization: AdaCore"
+    assert no_assertion.to_tagvalue() == "EntityRef: NOASSERTION"
+    assert str(no_assertion) == "NOASSERTION"
+    assert str(org) == "Organization: AdaCore"
+    assert no_assertion.to_json_dict() == {"entityRef": "NOASSERTION"}
+    assert org.to_json_dict() == {"entityRef": "Organization: AdaCore"}
 
 
 def test_external_ref():
@@ -62,41 +92,47 @@ def test_spdx():
             Person("e3-core maintainer"),
         ],
     )
-
-    doc.add_package(
-        name="my-spdx-test-main",
-        version="2.2.2",
-        file_name="main-pkg.zip",
+    package: Package = Package(
+        name=PackageName("my-spdx-test-main"),
+        version=PackageVersion("2.2.2"),
+        spdx_id=SPDXID("my-spdx-test-main-2.2.2"),
+        file_name=PackageFileName("main-pkg.zip"),
         checksum=[
             SHA1("6476df3aac780622368173fe6e768a2edc3932c8"),
             SHA256(
                 "91751cee0a1ab8414400238a761411daa29643ab4b8243e9a91649e25be53ada",
             ),
         ],
-        license_concluded="GPL-3.0-or-later",
-        license_declared="GPL-3.0-or-later",
-        supplier=Organization("AdaCore"),
-        originator=Organization("AdaCore"),
-        download_location=NOASSERTION,
-        files_analyzed=False,
-        copyright_text="2023 AdaCore",
-        is_main_package=True,
+        license_concluded=PackageLicenseConcluded("GPL-3.0-or-later"),
+        license_declared=PackageLicenseDeclared("GPL-3.0-or-later"),
+        license_comments=None,
+        supplier=PackageSupplier(Organization("AdaCore")),
+        originator=PackageOriginator(Organization("AdaCore")),
+        download_location=PackageDownloadLocation(NOASSERTION),
+        files_analyzed=FilesAnalyzed(False),
+        copyright_text=PackageCopyrightText("2023 AdaCore"),
+        external_refs=None,
+        homepage=None,
     )
 
-    doc.add_package(
-        name="my-dep",
-        version="1b2",
-        file_name="my-dep-1b2.tgz",
+    doc.add_package(package, is_main_package=True)
+
+    package = Package(
+        name=PackageName("my-dep"),
+        version=PackageVersion("1b2"),
+        spdx_id=SPDXID("my-dep-1b2"),
+        file_name=PackageFileName("my-dep-1b2.tgz"),
         checksum=[
             SHA1("6876df3aa8780622368173fe6e868a2edc3932c8"),
         ],
-        license_concluded="GPL-3.0-or-later",
-        license_comments="Pretty sure this is GPL v3",
-        supplier=Organization("AdaCore"),
-        originator=Organization("AdaCore"),
-        download_location=NOASSERTION,
-        files_analyzed=False,
-        copyright_text="2023 AdaCore",
+        license_concluded=PackageLicenseConcluded("GPL-3.0-or-later"),
+        license_declared=None,
+        license_comments=PackageLicenseComments("Pretty sure this is GPL v3"),
+        supplier=PackageSupplier(Organization("AdaCore")),
+        originator=PackageOriginator(Organization("AdaCore")),
+        download_location=PackageDownloadLocation(NOASSERTION),
+        files_analyzed=FilesAnalyzed(False),
+        copyright_text=PackageCopyrightText("2023 AdaCore"),
         external_refs=[
             ExternalRef(
                 reference_category=ExternalRefCategory.package_manager,
@@ -104,22 +140,33 @@ def test_spdx():
                 reference_locator="pkg:generic/my-dep@1b2",
             )
         ],
+        homepage=None,
+        comment=PackageComment("A very useful comment on that package !"),
     )
-    pkg_id = doc.add_package(
-        name="my-dep2",
-        version="1c3",
-        file_name="my-dep2-1c3.tgz",
+
+    doc.add_package(package)
+
+    package = Package(
+        name=PackageName("my-dep2"),
+        version=PackageVersion("1c3"),
+        spdx_id=SPDXID("my-dep2-1c3"),
+        file_name=PackageFileName("my-dep2-1c3.tgz"),
         checksum=[
             SHA1("6176df3aa1710633361173fe6e161a3edd3933d1"),
         ],
-        license_concluded="GPL-3.0-or-later",
-        supplier=Organization("AdaCore"),
-        originator=Organization("AdaCore"),
-        download_location=NOASSERTION,
-        files_analyzed=False,
-        copyright_text="2023 AdaCore",
-        add_relationship=False,
+        license_concluded=PackageLicenseConcluded("GPL-3.0-or-later"),
+        license_declared=None,
+        license_comments=None,
+        supplier=PackageSupplier(Organization("AdaCore")),
+        originator=PackageOriginator(Organization("AdaCore")),
+        download_location=PackageDownloadLocation(NOASSERTION),
+        files_analyzed=FilesAnalyzed(False),
+        copyright_text=PackageCopyrightText("2023 AdaCore"),
+        external_refs=None,
+        homepage=None,
     )
+
+    pkg_id = doc.add_package(package, add_relationship=False)
 
     doc.add_relationship(
         relationship=Relationship(
@@ -207,6 +254,7 @@ def test_spdx():
         "PackageLicenseComments: <text>Pretty sure this is GPL v3</text>",
         "PackageDownloadLocation: NOASSERTION",
         "ExternalRef: PACKAGE-MANAGER purl pkg:generic/my-dep@1b2",
+        "PackageComment: <text>A very useful comment on that package !</text>",
         "",
         "",
         "# Package",
@@ -286,6 +334,7 @@ def test_spdx():
                         "checksumValue": "6876df3aa8780622368173fe6e868a2edc3932c8",
                     }
                 ],
+                "comments": "A very useful comment on that package !",
                 "copyrightText": "2023 AdaCore",
                 "downloadLocation": "NOASSERTION",
                 "externalRefs": [
@@ -337,24 +386,29 @@ def test_invalid_spdx():
     )
 
     def add_main(is_main_package):
-        return doc.add_package(
-            name="my-spdx-test-main",
-            version="2.2.2",
-            file_name="main-pkg.zip",
+        package: Package = Package(
+            name=PackageName("my-spdx-test-main"),
+            version=PackageVersion("2.2.2"),
+            spdx_id=SPDXID("my-spdx-test-main-2.2.2"),
+            file_name=PackageFileName("main-pkg.zip"),
             checksum=[
                 SHA1("6476df3aac780622368173fe6e768a2edc3932c8"),
                 SHA256(
                     "91751cee0a1ab8414400238a761411daa29643ab4b8243e9a91649e25be53ada",
                 ),
             ],
-            license_concluded="GPL-3.0-or-later",
-            supplier=Organization("AdaCore"),
-            originator=Organization("AdaCore"),
-            download_location=NOASSERTION,
-            files_analyzed=False,
-            copyright_text="2023 AdaCore",
-            is_main_package=is_main_package,
+            license_concluded=PackageLicenseConcluded("GPL-3.0-or-later"),
+            license_declared=PackageLicenseDeclared("GPL-3.0-or-later"),
+            license_comments=None,
+            supplier=PackageSupplier(Organization("AdaCore")),
+            originator=PackageOriginator(Organization("AdaCore")),
+            download_location=PackageDownloadLocation(NOASSERTION),
+            files_analyzed=FilesAnalyzed(False),
+            copyright_text=PackageCopyrightText("2023 AdaCore"),
+            external_refs=None,
+            homepage=None,
         )
+        return doc.add_package(package, is_main_package=is_main_package)
 
     with pytest.raises(InvalidSPDX) as err:
         add_main(is_main_package=False)
@@ -369,21 +423,40 @@ def test_invalid_spdx():
                 name = "my-dep"
             else:
                 name = "my___-dep"
-            doc.add_package(
-                name=name,
-                version="1b2",
-                file_name="my-dep-1b2.tgz",
+            dep: Package = Package(
+                name=PackageName(name),
+                version=PackageVersion("1b2"),
+                spdx_id=SPDXID("my-dep-1b2"),
+                file_name=PackageFileName("my-dep-1b2.tgz"),
                 checksum=[
                     SHA1("6876df3aa8780622368173fe6e868a2edc3932c8"),
                 ],
-                license_concluded="GPL-3.0-or-later",
-                supplier=Organization("AdaCore"),
-                originator=Organization("AdaCore"),
-                download_location=NOASSERTION,
-                files_analyzed=False,
-                copyright_text="2023 AdaCore",
+                license_concluded=PackageLicenseConcluded("GPL-3.0-or-later"),
+                license_declared=None,
+                license_comments=None,
+                supplier=PackageSupplier(Organization("AdaCore")),
+                originator=PackageOriginator(Organization("AdaCore")),
+                download_location=PackageDownloadLocation(NOASSERTION),
+                files_analyzed=FilesAnalyzed(False),
+                copyright_text=PackageCopyrightText("2023 AdaCore"),
+                external_refs=None,
+                homepage=None,
             )
+            doc.add_package(dep)
     assert (
         "A package with the same SPDXID SPDXRef-my-dep-1b2 has already been added"
         in str(err)
     )
+
+
+def test_spdx_entry_maybe_str_multilines() -> None:
+    """SPDXEntryMaybeStrMultilines class tests.
+
+    Tests for the SPDXEntryMaybeStrMultilines class which are not covered by
+    the other tests.
+    """
+    ml: SPDXEntryMaybeStrMultilines = SPDXEntryMaybeStrMultilines("value")
+    no_assertion: SPDXEntryMaybeStrMultilines = SPDXEntryMaybeStrMultilines(NOASSERTION)
+
+    assert ml.to_tagvalue() == "SPDXEntryMaybeStrMultilines: <text>value</text>"
+    assert no_assertion.to_tagvalue() == "SPDXEntryMaybeStrMultilines: NOASSERTION"
