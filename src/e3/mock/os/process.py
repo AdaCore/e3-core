b287c020124fa38a80b3cdcc41c4430b66e0a8ff
Jérémy Morosi <morosi@adacore.com>
2024-03-05 11:08:02 +0100

    Add a mock_run function for mocking e3.os.process.Run
    Mocking e3.os.process.Run will allow to test code using that class
    without actually running the commands. Note that the goal is not
    to mock all functionalities but rather have a simple way to
    configure a list of expected commands and mock the result of each
    command, much like mocking requests

---
+++ src/e3/mock/os/process.py
@@ -0,0 +1,238 @@
+"""Submodule that mocks Run."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, overload
+from functools import wraps
+from unittest.mock import patch
+from threading import Lock
+import copy
+
+import e3.os.process
+from e3.os.process import Run, to_cmd_lines
+
+if TYPE_CHECKING:
+    from typing import Callable, Any, TypedDict
+    from typing_extensions import NotRequired, ParamSpec, TypeVar
+    from e3.os.process import AnyCmdLine
+
+    P = ParamSpec("P")
+    T = TypeVar("T")
+
+    class MockRunConfig(TypedDict):
+        """Config for MockRun."""
+
+        results: NotRequired[list[CommandResult]]
+        """Mocked results of expected commands"""
+
+
+class MockRunError(Exception):
+    """Error raised by mock_run."""
+
+    pass
+
+
+class UnexpectedCommandError(MockRunError):
+    """Error raised when MockRun receives an unexpected command."""
+
+    pass
+
+
+@overload
+def mock_run(func: Callable[P, T]) -> Callable[P, T]:
+    ...
+
+
+@overload
+def mock_run(func: None = None, config: MockRunConfig | None = None) -> RunPatcher:
+    ...
+
+
+def mock_run(
+    func: Callable[P, T] | None = None, config: MockRunConfig | None = None
+) -> RunPatcher | Callable[P, T]:
+    """Mock e3.os.process.Run as a context or decorator.
+
+    :param func: decorated function
+    :param config: config for the mock
+    :return: new RunPatcher instance or decorator
+    """
+    if func is not None:
+        return RunPatcher().__call__(func=func)
+    else:
+        return RunPatcher(config)
+
+
+class CommandResult:
+    """Result of a command.
+
+    When a command is supposed to be run, the actual status code and
+    logs are taken from this object.
+    """
+
+    def __init__(
+        self,
+        cmd: list[str],
+        status: int | None = None,
+        raw_out: bytes | None = None,
+        raw_err: bytes | None = None,
+    ) -> None:
+        """Initialize CommandResult.
+
+        :param cmd: expected arguments of the command
+        :param status: status code
+        :param raw_out: raw output log
+        :param raw_err: raw error log
+        """
+        self.cmd = cmd
+        self.status = status if status is not None else 0
+        self.raw_out = raw_out if raw_out is not None else b""
+        self.raw_err = raw_err if raw_err is not None else b""
+
+    def __call__(self) -> None:
+        """Allow to run code to emulate the command."""
+        pass
+
+
+class MockRun(Run):
+    """Mock e3.os.process.Run."""
+
+    def __init__(self, config: MockRunConfig | None = None) -> None:
+        """Initialize MockRun.
+
+        :param config: MockRun configuration
+        """
+        self.config: MockRunConfig = copy.deepcopy(config) if config is not None else {}
+
+    def add_result(self, result: CommandResult) -> None:
+        """Queue a command result.
+
+        :param result: new command result
+        """
+        self.config.setdefault("results", []).append(result)
+
+    def __call__(self, cmds: AnyCmdLine, *args: Any, **kwargs: Any) -> Run:
+        """Emulate how e3.os.process.Run.__init__ works.
+
+        Once e3.os.process.Run is patched, this function will be called instead.
+
+        :param cmds: command line
+        :param args: unhandled arguments
+        :param kwargs: unhandled keyword arguments
+        """
+        self.status: int | None = None
+        self.raw_out: bytes | None = b""
+        self.raw_err: bytes | None = b""
+
+        cmds = to_cmd_lines(cmds)
+        results: list[CommandResult] | None = self.config.get("results")
+
+        for cmd in cmds:
+            # Get next queued result
+            result = results.pop(0) if results else None
+            # Make sure we are expecting a command
+            if result is None:
+                raise UnexpectedCommandError(f"unexpected command {cmd}")
+
+            # Check arguments of the command
+            expected_cmd = result.cmd
+            if len(cmd) != len(expected_cmd):
+                raise UnexpectedCommandError(
+                    f"wrong number of arguments {cmd}, expected {expected_cmd}"
+                )
+
+            for i, arg in enumerate(cmd):
+                if arg != expected_cmd[i] and expected_cmd[i] != "*":
+                    raise UnexpectedCommandError(
+                        f"unexpected arguments {cmd}, expected {expected_cmd}"
+                    )
+
+            # Call the result
+            result()
+
+            # Update the output
+            self.status = result.status
+            self.raw_out += result.raw_out
+            self.raw_err += result.raw_err
+
+        return self
+
+
+class RunPatcher:
+    """Patch e3.os.process.Run when used as a context or decorator."""
+
+    _mock_run: MockRun | None = None
+    """Instance of MockRun."""
+    _nested_count = 0
+    """How many RunPatcher are currently alive."""
+    _mock_init_lock = Lock()
+    """Lock to avoid concurrency when patching/unpatching."""
+
+    def __init__(self, config: MockRunConfig | None = None) -> None:
+        """Initialize RunPatcher.
+
+        :param config: MockRun configuration
+        """
+        self.config = config
+
+    def __call__(self, func: Callable[P, T]) -> Callable[P, T]:
+        """Decorate func when mock_run is used as a decorator.
+
+        :param func: decorated function
+        :return: a decorator for calling func
+        """
+        return self._decorate_callable(func)
+
+    def __enter__(self) -> MockRun:
+        """Enter the context manager.
+
+        :return: MockRun instance
+        """
+        self.start()
+        assert self.__class__._mock_run is not None
+        return self.__class__._mock_run
+
+    def __exit__(self, *args: Any) -> None:
+        """Exit the context manager."""
+        self.stop()
+
+    def start(self) -> None:
+        """Start mocking e3.os.process.Run."""
+        with RunPatcher._mock_init_lock:
+            self.__class__._nested_count += 1
+            if self.__class__._nested_count == 1:
+                # Only apply the patch once
+                if isinstance(e3.os.process.Run, MockRun):
+                    raise MockRunError("e3.os.process.Run already patched")
+
+                self.__class__._mock_run = MockRun(config=self.config)
+                self._run_patch = patch("e3.os.process.Run", self.__class__._mock_run)
+                self._run_patch.start()
+
+    def stop(self) -> None:
+        """Stop mocking e3.os.process.Run."""
+        with RunPatcher._mock_init_lock:
+            self.__class__._nested_count -= 1
+            if self.__class__._nested_count < 0:
+                raise MockRunError("Called stop() before start().")
+
+            if self.__class__._nested_count == 0:
+                self.__class__._mock_run = None
+                self._run_patch.stop()
+
+    def _decorate_callable(self, func: Callable) -> Callable:
+        """Decorate the callable function.
+
+        :param func: decorated function
+        :return: a decorator for calling func
+        """
+
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Callable:
+            self.start()
+            try:
+                return func(*args, **kwargs)
+            finally:
+                self.stop()
+
+        return wrapper
---
+++ tests/tests_e3/mock/os/process/main_test.py
@@ -0,0 +1,152 @@
+"""Tests for e3.mock.os.process."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+import subprocess
+import time
+import pytest
+
+import e3.fs
+import e3.os.fs
+import e3.os.process
+from e3.mock.os.process import mock_run, CommandResult, MockRun, UnexpectedCommandError
+
+if TYPE_CHECKING:
+    from e3.mock.os.process import MockRunConfig
+
+# Mock the result of echo "hello"
+ECHO_HELLO_RESULT = CommandResult(["echo", '"hello"'], raw_out=b"hello\n")
+
+# Mock the result of echo "world"
+ECHO_WORLD_RESULT = CommandResult(["echo", '"world"'], raw_out=b"world\n")
+
+# Config for mock_run
+MOCK_RUN_CONFIG: MockRunConfig = {"results": [ECHO_HELLO_RESULT, ECHO_WORLD_RESULT]}
+
+
+class SleepCommandResult(CommandResult):
+    """CommandResult that emulates the sleep command."""
+
+    def __init__(self, seconds: float, /) -> None:
+        """Initialize SleepCommandResult.
+
+        :param seconds: seconds to sleep
+        """
+        self.seconds = seconds
+        super().__init__(["sleep", str(seconds)])
+
+    def __call__(self) -> None:
+        """Sleep x seconds."""
+        time.sleep(self.seconds)
+
+
+def echo(msg: str) -> None:
+    """Run echo "msg" with e3.os.process.Run.
+
+    :param msg: message to echo
+    """
+    assert isinstance(e3.os.process.Run, MockRun), "e3.os.process.Run not patched"
+    p = e3.os.process.Run(["echo", f'"{msg}"'], error=subprocess.PIPE)
+    assert p.out.replace("\r", "") == f"{msg}\n"
+
+
+def echo_hello() -> None:
+    """Run echo "hello" with e3.os.process.Run."""
+    echo("hello")
+
+
+def echo_world() -> None:
+    """Run echo "world" with e3.os.process.Run."""
+    echo("world")
+
+
+@mock_run()
+def test_mock_run_decorator() -> None:
+    """Test mock_run as a decorator without config."""
+    # Add results for both commands
+    for result in [ECHO_HELLO_RESULT, ECHO_WORLD_RESULT]:
+        e3.os.process.Run.add_result(result)
+
+    echo_hello()
+    echo_world()
+
+
+@mock_run(config=MOCK_RUN_CONFIG)
+def test_mock_run_decorator_config() -> None:
+    """Test mock_run as a decorator with config."""
+    echo_hello()
+    echo_world()
+
+
+def test_mock_run_context_config() -> None:
+    """Test mock_run as a context with config."""
+    with mock_run(config=MOCK_RUN_CONFIG):
+        # Run both commands
+        echo_hello()
+        echo_world()
+
+
+def test_mock_run_initial_add_result() -> None:
+    """Test add_result before running any command."""
+    with mock_run():
+        # Add results for both commands
+        for result in [ECHO_HELLO_RESULT, ECHO_WORLD_RESULT]:
+            e3.os.process.Run.add_result(result)
+
+        # Run both commands
+        echo_hello()
+        echo_world()
+
+
+def test_mock_run_sequential_add_result() -> None:
+    """Test add_result before running each command."""
+    with mock_run():
+        # Add the result for first command
+        e3.os.process.Run.add_result(ECHO_HELLO_RESULT)
+
+        # Run first command
+        echo_hello()
+
+        # Add the result for second command
+        e3.os.process.Run.add_result(ECHO_WORLD_RESULT)
+
+        # Run second command
+        echo_world()
+
+
+def test_mock_run_sleep() -> None:
+    """Test where the sleep command is emulated."""
+    with mock_run(config={"results": [SleepCommandResult(1)]}):
+        e3.os.process.Run(["sleep", "1"])
+
+
+def test_mock_run_unexpected_command() -> None:
+    """Test where a command is run while not expected."""
+    with mock_run(), pytest.raises(UnexpectedCommandError):
+        e3.os.process.Run(["sleep", "1"])
+
+
+@mock_run()
+def test_mock_run_wrong_command() -> None:
+    """Test where the wrong command is run."""
+    e3.os.process.Run.add_result(ECHO_HELLO_RESULT)
+
+    with pytest.raises(UnexpectedCommandError):
+        e3.os.process.Run(["time"])
+
+
+@mock_run()
+def test_mock_run_unexpected_arguments() -> None:
+    """Test where a command is run with unexpected arguments."""
+    e3.os.process.Run.add_result(ECHO_HELLO_RESULT)
+
+    with pytest.raises(UnexpectedCommandError):
+        e3.os.process.Run(["echo", '"world"'])
+
+
+def test_mock_run_multi_patch() -> None:
+    """Test that mock_run called multiple times is handled properly."""
+    with mock_run(), mock_run():
+        e3.os.process.Run.add_result(ECHO_HELLO_RESULT)
+        echo_hello()
