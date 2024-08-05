975a29670ab65969d56d657df3b4a88c01ddaf39
Pat Bernardi <bernardi@adacore.com>
2024-08-05 16:04:04 -0400

    Add aarch64-freebsd to platform knowledge base
    Issue: eng/toolchain/gnat#1014

--- src/e3/platform_db/knowledge_base.py
+++ src/e3/platform_db/knowledge_base.py
@@ -80,6 +80,7 @@ OS_INFO: PlatformDBEntry = {
 PLATFORM_INFO: PlatformDBEntry = {
     "aarch64-darwin": {"cpu": "aarch64", "os": "darwin", "is_hie": False},
     "aarch64-elf": {"cpu": "aarch64", "os": "none", "is_hie": True},
+    "aarch64-freebsd": {"cpu": "aarch64", "os": "freebsd", "is_hie": False},
     "aarch64-ios": {"cpu": "aarch64", "os": "ios", "is_hie": False},
     "aarch64-linux": {"cpu": "aarch64", "os": "linux", "is_hie": False},
     "arm-android": {"cpu": "arm", "os": "android", "is_hie": False},
@@ -118,6 +119,7 @@ PLATFORM_INFO: PlatformDBEntry = {
 BUILD_TARGETS: PlatformDBEntry = {
     "aarch64-darwin": {"name": "aarch64-apple-darwin%(os_version)s"},
     "aarch64-elf": {"name": "aarch64-elf"},
+    "aarch64-freebsd": {"name": "aarch64-freebsd%(os_version)s"},
     "aarch64-ios": {"name": "aarch64-apple-darwin"},
     "aarch64-linux": {"name": "aarch64-linux-gnu"},
     "arm-android": {"name": "arm-linux-androideabi"},
@@ -160,6 +162,7 @@ BUILD_TARGETS: PlatformDBEntry = {
 HOST_GUESS: PlatformDBEntry = {
     # platform : OS (uname[0]), machine (uname[1]), proc (uname[4 or 5])
     "aarch64-darwin": {"os": "Darwin", "cpu": "arm64"},
+    "aarch64-freebsd": {"os": "FreeBSD", "cpu": "arm64"},
     "aarch64-linux": {"os": "Linux", "cpu": "aarch64"},
     "ppc-aix": {"os": "AIX", "cpu": None},
     "x86_64-darwin": {"os": "Darwin", "cpu": "i386"},
