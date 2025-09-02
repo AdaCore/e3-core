26c1237cac475a91763f4fd8cfc272b40973b5e0
Léo Cardao <cardao@adacore.com>
2025-09-02 10:18:14 +0200

    Rework e3.maven
    This commit reworks `e3.maven` to use the same searching method as Maven,
    instead of using the `Solrsearch` API.
    
    This implies downloading the `maven-metadata.xml` file, referencing all
    usable versions of a given package.
    
    This commit also adds some testing for the `e3.maven` module. To do this,
    we have created a fixture to mock a Maven central repository.
    
    All URLs used have been replaced with https://repo.maven.apache.org/maven2,
    if they were different before.
    
    Part of it/package-registry#27
    Part of it/e3-core#49

---
+++ tests/tests_e3/maven_test.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from e3.maven import Maven
+
+
+def test_maven(maven_central):
+    mvn = Maven()
+
+    maven_central.register_package(
+        group="test.e3.mvn",
+        name="e3mvn",
+        versions={
+            "6.1.17",
+            "6.1.18",
+            "6.1.19",
+            "6.1.20",
+        },
+    )
+    with maven_central:
+        links = mvn.fetch_project_links("test.e3.mvn", "e3mvn")
+
+    assert len(links) == 4
+    assert all(
+        link.version
+        in {
+            "6.1.17",
+            "6.1.18",
+            "6.1.19",
+            "6.1.20",
+        }
+        for link in links
+    )
+
+    assert all(link.sha1_checksum and link.md5_checksum for link in links)
+
+    for link in links:
+        data = maven_central.get_package_data(
+            link.package_group, link.package_name, link.version
+        )
+        assert link.sha1_checksum == data["sha1"]
+        assert link.md5_checksum == data["md5"]
+
+        data = maven_central.get_pom_data(
+            link.package_group, link.package_name, link.version
+        )
+        assert link.pom_sha1_checksum == data["sha1"]
+        assert link.pom_md5_checksum == data["md5"]
