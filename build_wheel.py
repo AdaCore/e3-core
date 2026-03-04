e438e7c535d7939e66127145c1a0f12f0bc451bf
cookiecutter-e3 <cookiecutter-e3@adacore.com>
2026-03-04 11:13:16 +0000

    Update build_wheel.py and tox.ini for ruff
    Take into account latest e3-core changes on build_wheel.py and tox.ini regarding
    the switch from flake8 and black to ruff.
    
    Ref: it/org/software-supply-chain/production-pipeline/issues#193
    
    See merge request it/cookiecutter-e3!105 of the template

--- build_wheel.py
+++ build_wheel.py
@@ -80,7 +80,7 @@ def main() -> None:
         sys.exit(1)
 
     # Find the path to version file
-    with open(project, "rb") as f:
+    with project.open("rb") as f:
         version_config = (
             tomllib.load(f)
             .get("tool", {})
@@ -103,7 +103,7 @@ def main() -> None:
     # Read the version
     root_dir = project.parent
     version_abspath = root_dir / version_path
-    with open(version_abspath) as f:
+    with version_abspath.open() as f:
         version_content = f.read()
 
     # Extract the <major>.<minor>(.<patch>)? part.
@@ -165,7 +165,7 @@ def main() -> None:
 
     if not main.args.dry_run:
         # Replace the version in the file
-        with open(version_abspath, "w") as f:
+        with version_abspath.open("w") as f:
             f.write(version_content.replace(version, build_version))
 
         try:
