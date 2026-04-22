0ad28774c4cb864eb486e73bcd90f46eeecd24f0
Liaiss Merzougue <merzougue@adacore.com>
2026-04-22 08:58:11 +0000

    Fix N802 rule
    Add # noqa as getLogger is used to match the logging api
    name
    
    Ref: it/org/software-supply-chain/production-pipeline/issues#204

--- src/e3/log.py
+++ src/e3/log.py
@@ -329,7 +329,9 @@ class TqdmHandler(logging.StreamHandler):  # all: no cover
         tqdm.write(msg, file=sys.stderr)
 
 
-def getLogger(name: str | None = None, prefix: str = "e3") -> E3LoggerAdapter:
+def getLogger(  # noqa: N802
+    name: str | None = None, prefix: str = "e3"
+) -> E3LoggerAdapter:
     """Get a logger with a default handler doing nothing.
 
     Calling this function instead of logging.getLogger will avoid warnings
